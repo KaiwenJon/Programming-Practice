ca1c867e8a2db93119396bc9a6bd77dcd5d1ca71
KaiwenJon <kaiwenjon23@gmail.com>
2021-03-21 19:02:40 +0800

    Implement sorting using STL interface.

--- week1/main.cpp
+++ week1/main.cpp
@@ -1,49 +1,62 @@
 #include <iostream>
 #include <stdio.h>
-#include <stdlib.h> 
-#include <time.h> 
+#include <stdlib.h>
+#include <time.h>
 
 #include "selectionSort.h"
-#include "bubbleSort.h" 
+#include "bubbleSort.h"
 #include "insertionSort.h"
 
 typedef int DataType;
 
 int compare(const void *a, const void *b)
 {
-      int c = *(int *)a;
-      int d = *(int *)b;
-      if(c < d) {return -1;}
-      else if (c == d) {return 0;} 
-      else return 1; 
+	int c = *(int *)a;
+	int d = *(int *)b;
+	if (c < d)
+	{
+		return -1;
+	}
+	else if (c == d)
+	{
+		return 0;
+	}
+	else
+		return 1;
 }
 
-int main(int argc, char** argv) {
-
+int main(int argc, char **argv)
+{
+	int b = 5;
+	int *a = &b;
+	size_t es = sizeof(b);
+	int swaptype = ((char *)a - (char *)0) % sizeof(long) || es % sizeof(long) ? 2
+				   : es == sizeof(long)										   ? 0
+																			   : 1;
+	std::cout << (sizeof(long));
 	int num, i;
-	std::cout << "Please enter the size of the array:" << std::endl; 
+	std::cout << "Please enter the size of the array:" << std::endl;
 	std::cin >> num;
-	
+
 	int *array = new int[num];
-	srand((unsigned) time(NULL));
-	for(i=0;i<num;i++)
-		array[i]=rand()%100;
-		
-	for(unsigned i = 0; i < num; i++)
+	srand((unsigned)time(NULL));
+	for (i = 0; i < num; i++)
+		array[i] = rand() % 100;
+
+	for (unsigned i = 0; i < num; i++)
 	{
-	 printf(" %d\t", array[i]);
+		printf(" %d\t", array[i]);
 	}
 	printf("\n\n");
-	
-//	qsort((void *)array, num, sizeof(array[0]), compare);
-//	selectionSort(array, num);
-//	bubbleSort(array, num);
+
+	//	qsort((void *)array, num, sizeof(array[0]), compare);
+	//	selectionSort(array, num);
+	//	bubbleSort(array, num);
 	insertionSort(array, num);
-	
-	
-	for(unsigned i = 0; i < num; i++)
+
+	for (unsigned i = 0; i < num; i++)
 	{
-	 printf(" %d\t", array[i]);
+		printf(" %d\t", array[i]);
 	}
 	delete[] array;
 	system("PAUSE");
---
+++ week4/main.cpp
@@ -0,0 +1,55 @@
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "my_sorting_lib.h"
+
+int compare(const void *a, const void *b)
+{
+    int c = *(int *)a;
+    int d = *(int *)b;
+    if (c < d)
+    {
+        return -1;
+    }
+    else if (c == d)
+    {
+        return 0;
+    }
+    else
+        return 1;
+}
+
+int main(int argc, char **argv)
+{
+
+    int num, i;
+    std::cout << "Please enter the size of the array:" << std::endl;
+    std::cin >> num;
+
+    int *array = new int[num];
+    srand((unsigned)time(NULL));
+    for (i = 0; i < num; i++)
+        array[i] = rand() % 100;
+
+    // Random numbers
+    for (unsigned i = 0; i < num; i++)
+    {
+        printf(" %d\t", array[i]);
+    }
+    printf("\n\n");
+
+    //Sorting Algorithm
+    // myqsort((void *)array, num, sizeof(array[0]), compare);
+    // mybubbleSort((void *)array, num, sizeof(array[0]), compare);
+    myinsertionSort((void *)array, num, sizeof(array[0]), compare);
+    std::cout << "After Sorting....\n";
+    //Sorted Array
+    for (unsigned i = 0; i < num; i++)
+    {
+        printf(" %d\t", array[i]);
+    }
+    delete[] array;
+    system("PAUSE");
+}
